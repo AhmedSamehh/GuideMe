45389af8cfbb65f1eb9e851d6ab99c7848dca70d
Ahmad Sameh <41856063+AhmedSamehh@users.noreply.github.com>
2021-10-14 19:08:12 +0200

    Registeration endpoint

---
+++ GuideMe/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using GuideMe.Models;
+using GuideMe.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuideMe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await _authService.RegisterAsync(model);
+            if (!result.IsAuthenticated)
+                return BadRequest(result.Message);
+            return Ok(result);
+        }
+    }
+}
